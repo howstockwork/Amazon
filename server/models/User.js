b646aac7246f046092fd6bb4d7d5d888e7d391a7
Mufutaw Hamed <mufutawhamed@gmail.com>
2026-02-25 10:01:31 +0100

    Create User model with schema

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    preferences: {
+        type: Object
+    }
+});
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
